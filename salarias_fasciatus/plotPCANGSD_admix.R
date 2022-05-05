aa1722b7a9d29b419a0b43991ed40469326e1c8c
jrodriguez216 <jrodriguez216@islander.tamucc.edu>
2022-05-05 13:44:58 -0500

    update using runGIT.bash

--- salarias_fasciatus/plotPCANGSD_admix.R
+++ salarias_fasciatus/plotPCANGSD_admix.R
@@ -11,6 +11,7 @@ library(RcppCNPy) # Numpy library for R
 pop<-read.table("fltrBAM/popmap_sfa.tsv",
                 header = TRUE)
 
+
 q <- npyLoad("PCAngsd_admix/out_PCAngsd_admix.admix.3.F.npy") 
 
 ## order according to population
@@ -19,4 +20,5 @@ barplot(t(q)[,ord],col=2:10,space=0,border=NA,xlab="Individuals",ylab="Admixture
 text(tapply(1:nrow(pop),pop[ord,1],mean),-0.05,unique(pop[ord,1]),xpd=T)
 abline(v=cumsum(sapply(unique(pop[ord,1]),function(x){sum(pop[ord,1]==x)})),col=1,lwd=1.2)
 
+ggsave("./PCAngsd_admix.png", units = "in", height = 4, width = 6)
 ## close R

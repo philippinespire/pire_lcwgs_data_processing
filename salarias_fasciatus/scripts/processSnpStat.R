796f3509744b8c5fd81b5e3131560d7b9ea837f2
cbird808 <cbird808@gmail.com>
2022-05-20 11:14:31 -0500

    update using runGIT.bash

--- salarias_fasciatus/scripts/processSnpStat.R
+++ salarias_fasciatus/scripts/processSnpStat.R
@@ -11,6 +11,7 @@ inFILE="../mkBGL/test03.100k.snpStat"
 numInd=81
 minInd=41
 maxDP=numInd*5
+sb_pval=0.5
 
 #### WRANGLE SNP STATS ####
 data_snp <-
@@ -77,7 +78,12 @@ data_snp <-
            into = c("het_stat", 
                     "het_stat_pval"),
            convert = TRUE,
-           sep = ":") 
+           sep = ":") %>%
+  mutate(pos_allele_balance = pos_minor_het / (pos_minor_het + pos_major_het),
+         neg_allele_balance = neg_minor_het / (neg_minor_het + neg_major_het),
+         allele_balance = (pos_minor_het + neg_minor_het) / (pos_minor_het + pos_major_het + neg_minor_het + neg_major_het),
+         rough_est_het = (pos_minor_het + pos_major_het + neg_minor_het + neg_major_het)/(pos_minor + pos_major + neg_minor + neg_major))
+
 
 pos_minor_prop_max = max(data_snp$pos_minor_prop,
                           na.rm = TRUE)
@@ -88,57 +94,96 @@ pos_minor_prop_max = max(data_snp$pos_minor_prop,
 #           sep=""))
   data_snp %>%
     ggplot(aes(x=pos_minor+pos_major)) +
-    geom_histogram(bins = 101) +
+    geom_histogram(bins = 101,
+                   fill = "white",
+                   color = "grey") +
     geom_vline(xintercept = minInd,
                linetype = "dashed",
                color = "red3") +
     geom_vline(xintercept = maxDP,
                linetype = "dashed",
                color = "red3") +
+    geom_text(aes(y = 0.5,
+                  x = minInd,
+                  label = "-minInd * -setMinDepthInd"), 
+              vjust = "inward", 
+              hjust = "outward") +
+    geom_text(aes(y = 0.5,
+                  x = maxDP,
+                  label = "-setMaxDepth"), 
+              vjust = "inward", 
+              hjust = "outward") +
     scale_y_continuous(trans='log10') +
     scale_x_continuous(trans='log10') +
     theme_classic() +
     labs(title = "Positive Strand Read Counts",
-         subtitle = "Threshold = minInd (min 1 read per ind)") 
+         subtitle = "Use to help determine -minInd, -setMinDepthInd, -setMaxDepth\nNote: axes log10 scaled") 
   
   data_snp %>%
     ggplot(aes(x=neg_minor+neg_major)) +
-    geom_histogram(bins = 101) +
+    geom_histogram(bins = 101,
+                   fill = "white",
+                   color = "grey") +
     geom_vline(xintercept = minInd,
                linetype = "dashed",
                color = "red3") +
     geom_vline(xintercept = maxDP,
                linetype = "dashed",
                color = "red3") +
+    geom_text(aes(y = 0,
+                  x = minInd,
+                  label = "-minInd * -setMinDepthInd"), 
+              vjust = "outward", 
+              hjust = "outward") +
+    geom_text(aes(y = 0,
+                  x = maxDP,
+                  label = "-setMaxDepth"), 
+              vjust = "outward", 
+              hjust = "outward") +
+    
     # scale_y_continuous(trans='log10') +
     scale_x_continuous(trans='log10') +
     theme_classic() +
     labs(title = "Negative Strand Read Counts",
-         subtitle = "Lower Threshold = minInd (min 1 read per ind)") 
+         subtitle = "Use to help determine -minInd, -setMinDepthInd, -setMaxDepth\nNote: only x axis log10 scaled") 
   
   data_snp %>%
     ggplot(aes(x=pos_minor_prop)) +
-    geom_histogram(bins = 101) +
+    geom_histogram(bins = 101,
+                   fill = "white",
+                   color = "grey") +
     geom_vline(xintercept = 1 / (numInd*2),
                linetype = "dashed",
                color = "red3") +
+    geom_text(aes(y = 0.5,
+                  x = 1 / (numInd*2),
+                  label = "-minMaf"), 
+              vjust = "inward", 
+              hjust = "outward") +
     scale_y_continuous(trans='log10') +
     scale_x_continuous(trans='log10') +
     theme_classic() +
     labs(title = "Positive Strand Minor AF",
-         subtitle = "Threshold = 1 Minor Allele") 
+         subtitle = "Use to set -minMaf\nThreshold displayed = 1 Minor Allele in numInd diploids\nPeak is indicative of resolution based on combination of sample size and number of reads per sample\nIf peak is at Threshold, there is little to no missing data across all sites and the meanDP > 2 reads") 
   
   data_snp %>%
     ggplot(aes(x=neg_minor_prop)) +
-    geom_histogram(bins = 101) +
+    geom_histogram(bins = 101,
+                   fill = "white",
+                   color = "grey") +
     geom_vline(xintercept = 1 / (numInd*2),
                linetype = "dashed",
                color = "red3") +
+    geom_text(aes(y = 0,
+                  x = 1 / (numInd*2),
+                  label = "-minMaf"), 
+              vjust = "outward", 
+              hjust = "outward") +
     # scale_y_continuous(trans='log10') +
     scale_x_continuous(trans='log10') +
     theme_classic() +
     labs(title = "Negative Strand Minor AF",
-         subtitle = "Threshold = 1 Minor Allele") 
+         subtitle = "Use to set -minMaf\nThreshold displayed = 1 Minor Allele in numInd diploids\nPeak is indicative of resolution based on combination of sample size and number of reads per sample\nIf peak is at Threshold, there is little to no missing data across all sites and the meanDP > 2 reads\Note: yaxis not log10") 
   
   data_snp %>%
     ggplot(aes(x=as.numeric(strand_bias_1))) +
@@ -157,14 +202,21 @@ pos_minor_prop_max = max(data_snp$pos_minor_prop,
   
   data_snp %>%
     ggplot(aes(x=strand_bias_3)) +
-    geom_histogram() +
-    geom_vline(xintercept = 0.5,
+    geom_histogram(fill = "white",
+                   color = "grey") +
+    geom_vline(xintercept = sb_pval,
                linetype = "dashed",
                color = "red3") +
+    geom_text(aes(y = 0,
+                  x = sb_pval,
+                  label = "-sb_pval"), 
+              vjust = "outward", 
+              hjust = "inward") +
     # scale_y_continuous(trans='log10') +
     scale_x_continuous(trans='log10') +
     theme_classic() +
-    labs(title = "Strand Bias 3") 
+    labs(title = "Strand Bias 3",
+         subtitle = "Use to set -sb_pval, lower is more permissive, higher is more restrictive\n strand bias is when there is disagreement between read1 and read2 in the state of a site") 
   
   data_snp %>%
     ggplot(aes(x=hwe_pval)) +
@@ -221,4 +273,82 @@ pos_minor_prop_max = max(data_snp$pos_minor_prop,
     scale_x_continuous(trans='log10') +
     theme_classic() +
     labs(title = "Het Stat P Value") 
+  
+    ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # these require het bias filter to be set when bgl file was made
+    data_snp %>%
+    ggplot(aes(x=het_stat_pval)) +
+    geom_histogram() +
+    geom_vline(xintercept = 0.1,
+               linetype = "dashed",
+               color = "red3") +
+    # scale_y_continuous(trans='log10') +
+    scale_x_continuous(trans='log10') +
+    theme_classic() +
+    labs(title = "Het Stat P Value")
+
+    data_snp %>%
+    ggplot(aes(x=pos_allele_balance)) +
+    geom_histogram() +
+    geom_vline(xintercept = 0.375,
+               linetype = "dashed",
+               color = "red3") +
+    geom_vline(xintercept = 0.675,
+               linetype = "dashed",
+               color = "red3") +
+    # scale_y_continuous(trans='log10') +
+    scale_x_continuous(trans='log10') +
+    theme_classic() +
+    labs(title = "Pos Allele Balance")
+
+    data_snp %>%
+    ggplot(aes(x=neg_allele_balance)) +
+    geom_histogram() +
+    geom_vline(xintercept = 0.375,
+               linetype = "dashed",
+               color = "red3") +
+    geom_vline(xintercept = 0.675,
+               linetype = "dashed",
+               color = "red3") +
+    # scale_y_continuous(trans='log10') +
+    scale_x_continuous(trans='log10') +
+    theme_classic() +
+    labs(title = "Neg Allele Balance")
+
+  
+    data_snp %>%
+    ggplot(aes(x=allele_balance)) +
+    geom_histogram() +
+    geom_vline(xintercept = 0.375,
+               linetype = "dashed",
+               color = "red3") +
+    geom_vline(xintercept = 0.675,
+               linetype = "dashed",
+               color = "red3") +
+    # scale_y_continuous(trans='log10') +
+    scale_x_continuous(trans='log10') +
+    theme_classic() +
+    labs(title = "Mean Allele Balance")
+
+  
+ 
+    data_snp %>%
+    ggplot(aes(y=rough_est_het,
+               x=allele_balance)) +
+    geom_point() +
+    geom_vline(xintercept = 0.375,
+               linetype = "dashed",
+               color = "red3") +
+    geom_vline(xintercept = 0.675,
+               linetype = "dashed",
+               color = "red3") +
+    geom_hline(yintercept = 0.5,
+               linetype = "dashed",
+               color = "red3") +
+    # scale_y_continuous(trans='log10') +
+    scale_x_continuous(trans='log10') +
+    theme_classic() +
+    labs(title = "Rough Estimate of Heterozygosty vs Mean Allele Balance")
+
+  
 # dev.off()

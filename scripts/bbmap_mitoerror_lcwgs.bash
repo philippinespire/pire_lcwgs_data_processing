2253f1169716d1759f6425811f770363abc4f3bb
nerdbrained <nerdbrained@gmail.com>
2022-12-07 13:30:00 -0500

    bbmap_mitoerror fix path

--- scripts/bbmap_mitoerror_lcwgs.bash
+++ scripts/bbmap_mitoerror_lcwgs.bash
@@ -2,7 +2,6 @@
 
 #script to get error distributions from sequence data and mitochondrial genomes extracted from lcwgs
 
-
 SPDIR=$1		#species directory. example= /home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/salarias_fasciatus
 FQDIR=$2		#directory with reads. example= fq_fp1_clmp_fp2_fqscrn_rprd
 
@@ -10,8 +9,7 @@ FQPATTERN=*_clmp.fp2_repr.R1.fq.gz
 
 SCRIPTPATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-all_samples=$(ls $SPDIR/mkBAM/$FQPATTERN | sed -e 's/_clmp.fp2_repr.R1\.fq/\.gz/' -e 's/.*\///g')
+all_samples=$(ls $SPDIR/$FQDIR/$FQPATTERN | sed -e 's/_clmp.fp2_repr.R1.fq.gz//' -e 's/.*\///g')
 all_samples=($all_samples)
-
 
 sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} $SCRIPTPATH/bbmap_mitoerror_lcwgs.sbatch ${SPDIR} ${FQDIR}

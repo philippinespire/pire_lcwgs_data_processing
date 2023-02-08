88e65dc94996f71be4900366e52233df1435c6db
nerdbrained <nerdbrained@gmail.com>
2023-02-08 10:21:53 -0500

    filter_pp fix paths

--- scripts/filter_pp.bash
+++ scripts/filter_pp.bash
@@ -3,8 +3,9 @@
 #script to filter BAM files for proper pairs + remove duplicates
 #for use on existing .bam files
 
-#INDIR=./mkBAM/
-#OUTDIR=./fltrBAM_pp
+#SPDIR=/home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/salarias_fasciatus/1st_sequencing_run
+#INDIR=mkBAM/
+#OUTDIR=fltrBAM_pp
 #THREADS=40
 
 SPDIR=$1

e94b3746f85ffea4f7b023823c0e4e2266438e22
nerdbrained <nerdbrained@gmail.com>
2023-08-07 16:04:06 -0400

    update PALEO_makemakefile

--- scripts/PALEO_makemakefile.sh
+++ scripts/PALEO_makemakefile.sh
@@ -1,3 +1,7 @@
+# currently throwing some errors related missing individuals in particular runs but producing a proper makefile
+# to run use sh PALEO_makemakefile.sh [species lcwgs directory] [subdirectory within this to use for PALEOMIX processing/output] [species code] [absolute path to reference location] [absolute path to decode file with all individuals to use] [number of sequencing runs to process; currently "one", "two", or "three"]
+# after makefile has been produced navigate to PALEODIR and run PALEOMIX sbatch script: sbatch home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/scripts/run_PALEOMIX.sbatch
+
 SPDIR=$1
 PALEODIR=$2
 SPCODE=$3
@@ -16,12 +20,13 @@ echo "  ${SPCODE}:" >> makefile.yaml
 echo "    Path: ${REFLOC}" >> makefile.yaml
 echo -e "\n" >> makefile.yaml
 
+echo "${SPCODE}_lcWGS:" >> makefile.yaml
+
 cut -d'	' -f2 ${DECODE} | cut -c 1-12 > indnames.txt
 
 if [ $NRUNS = "one" ]
 then
   sed 1d indnames.txt | while read ind; do
-  echo "${ind}:"
   echo "  ${ind}:"
   echo "    ${ind}_Lib1:"
   filename1=$(ls /home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/salarias_fasciatus/1st_sequencing_run/fq_raw/*1.fq.gz | grep "${ind}" | sed 's/1.fq.gz/{Pair}.fq.gz/g')
@@ -33,14 +38,16 @@ fi
 if [ $NRUNS = "two" ]
 then
   sed 1d indnames.txt | while read ind; do
-  echo "${ind}:"
   echo "  ${ind}:"
   echo "    ${ind}_Lib1:"
   filename1=$(ls /home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/salarias_fasciatus/1st_sequencing_run/fq_raw/*1.fq.gz | grep "${ind}" | sed 's/1.fq.gz/{Pair}.fq.gz/g')
   echo "      Lane_1: ${filename1}"
-  echo "    ${ind}_Lib2:"
   filename2=$(ls /home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/salarias_fasciatus/2nd_sequencing_run/fq_raw/*1.fq.gz | grep "${ind}" | sed 's/1.fq.gz/{Pair}.fq.gz/g')
-  echo "      Lane_1: ${filename2}"
+  if [ $filename2 != "" ] 
+  then 
+    echo "    ${ind}_Lib2:"
+    echo "      Lane_1: ${filename2}" 
+  fi
   echo -e "\n"
 done >> makefile.yaml
 fi
@@ -48,17 +55,22 @@ fi
 if [ $NRUNS = "three" ]
 then
   sed 1d indnames.txt | while read ind; do
-  echo "${ind}:"
   echo "  ${ind}:"
   echo "    ${ind}_Lib1:"
   filename1=$(ls /home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/salarias_fasciatus/1st_sequencing_run/fq_raw/*1.fq.gz | grep "${ind}" | sed 's/1.fq.gz/{Pair}.fq.gz/g')
   echo "      Lane_1: ${filename1}"
-  echo "    ${ind}_Lib2:"
   filename2=$(ls /home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/salarias_fasciatus/2nd_sequencing_run/fq_raw/*1.fq.gz | grep "${ind}" | sed 's/1.fq.gz/{Pair}.fq.gz/g')
-  echo "      Lane_1: ${filename2}"
-  echo "    ${ind}_Lib3:"
+  if [ $filename2 != "" ] 
+  then 
+    echo "    ${ind}_Lib2:"
+    echo "      Lane_1: ${filename2}" 
+  fi
   filename3=$(ls /home/e1garcia/shotgun_PIRE/pire_lcwgs_data_processing/salarias_fasciatus/3rd_sequencing_run/fq_raw/*1.fq.gz | grep "${ind}" | sed 's/1.fq.gz/{Pair}.fq.gz/g')
-  echo "      Lane_1: ${filename3}"
+  if [ $filename3 != "" ]
+  then
+    echo "    ${ind}_Lib3:"
+    echo "      Lane_1: ${filename3}"
+  fi
   echo -e "\n"
 done >> makefile.yaml
 fi

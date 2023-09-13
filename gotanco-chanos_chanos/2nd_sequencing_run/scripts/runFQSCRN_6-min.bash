faca3121af3668e9615d7d8b0a964c48f47439de
kllabrador <kllabrador@up.edu.ph>
2023-09-13 11:01:13 -0400

    files transferred

---
+++ gotanco-chanos_chanos/2nd_sequencing_run/scripts/runFQSCRN_6-min.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# this script sends several jobs each to their own compute node using an array, which limits the number of nodes used at one time
+
+INDIR=$1
+OUTDIR=$2
+#TMPDIR=$3
+NUMNODES=$3   # 5 to 10 on tamucc
+FQPATTERN=$4
+
+if [[ -z "$FQPATTERN" ]]; then
+       FQPATTERN=*.fq.gz
+fi
+mkdir $OUTDIR
+
+all_samples=( $(ls $INDIR/$FQPATTERN) )
+
+JOBID=$(sbatch --array=0-$((${#all_samples[@]}-1))%${NUMNODES} \
+       --output=slurm-fqscrn.%A.%a.out \
+       --partition main \
+       -t 96:00:00 \
+       /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runFQSCRN_6_min.sbatch ${INDIR} ${OUTDIR} ${FQPATTERN})
+       
+# NUMBER1=$(echo ${JOBID} | sed 's/[^0-9]*//g')
+# sbatch --dependency=afterok:${NUMBER1} /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch $OUTDIR fqscrn_mqc
